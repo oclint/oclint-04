e3868535d37f2a7808765628cf1b2c14b2e5da4b
Longyi Qi <lqi@uh.edu>
2011-01-28 10:38:14 -0600

    add more tests to make sure the new test framework works

--- test/MO_RULE_ASTVisitor.h
+++ test/MO_RULE_ASTVisitor.h
@@ -1,6 +1,39 @@
 #include <cxxtest/TestSuite.h>
 #include "mo/rule/ASTVisitor.h"
 
+#include <clang/AST/Stmt.h>
+#include <clang/AST/DeclBase.h>
+
+using namespace clang;
+
+class TestStmt : public Stmt {
+public:
+  TestStmt() : Stmt(NullStmtClass) {}
+
+public:  
+  child_iterator child_begin() {
+    return StmtIterator();
+  }
+  
+  child_iterator child_end() {
+    return StmtIterator();
+  }
+  
+  SourceRange getSourceRange() const {
+    return SourceRange(SourceLocation(), SourceLocation());
+  }
+};
+
+class TestDeclContext : public DeclContext {
+public:
+  TestDeclContext() : DeclContext(Decl::Var) {}
+};
+
+class TestDecl : public Decl {
+public:
+  TestDecl() : Decl(Var, new TestDeclContext(), SourceLocation()) {}
+};
+
 class ASTVisitorTest : public CxxTest::TestSuite { 
 private:
   ASTVisitor *visitor;
@@ -13,9 +46,12 @@ public:
     delete visitor;
   }
   
-  void testFireGCov() {
-    SourceLocation loc;
-    NullStmt stmt(loc);
+  void testVisitorTraverseStmt() {
+    TestStmt stmt;
     TS_ASSERT(visitor->traverse(&stmt));
+  }
+  
+  void testVisitorTraverseDecl() {
+    TS_ASSERT(!visitor->traverse(new TestDecl()));
   }
 };
