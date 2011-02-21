0fed98162f9663a0ae527fc147319dd8e4833760
Longyi Qi <lqi@uh.edu>
2011-02-20 21:44:35 -0600

    remove two lines of useless code

--- test/impl/mo/rule/SwitchStatementRuleTest.cpp
+++ test/impl/mo/rule/SwitchStatementRuleTest.cpp
@@ -22,11 +22,8 @@ void SwitchStatementRuleTest::tearDown() {
   delete rule;
 }
 
-#include "mo/util/CursorUtil.h"
-
 void SwitchStatementRuleTest::testApply() {
   RuleData data;
-  Stmt *stmt = CursorUtil::getStmt(stmtCursor());
   rule->apply(stmtCursor(), clang_getNullCursor(), data);
   TS_ASSERT_EQUALS(data.numberOfViolations(), 1);
 }
