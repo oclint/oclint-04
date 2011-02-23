facf303c5a1d9ffe75737ee0f2af2dfc6a8cb3df
Longyi Qi <lqi@uh.edu>
2011-02-23 15:47:53 -0600

    add implementation for plain text diagnostics report

--- src/impl/mo/reporter/PlainTextReporter.cpp
+++ src/impl/mo/reporter/PlainTextReporter.cpp
@@ -4,7 +4,14 @@
 #include <iostream>
 
 void PlainTextReporter::reportDiagnostics(vector<CXDiagnostic> diagnostics) const {
-  cout << "Diagnostics Detected!" << endl;
+  unsigned displayOptions = CXDiagnostic_DisplaySourceLocation
+    | CXDiagnostic_DisplayColumn | CXDiagnostic_DisplaySourceRanges
+    | CXDiagnostic_DisplayOption;
+  for (int index = 0, numberOfDiagnostics = diagnostics.size(); index < numberOfDiagnostics; index++) {
+    CXString diagnostic = clang_formatDiagnostic(diagnostics.at(index), displayOptions);
+    cout << clang_getCString(diagnostic) << endl;
+    clang_disposeString(diagnostic);
+  }
 }
 
 void PlainTextReporter::reportViolations(vector<RuleViolation> violations) const {
