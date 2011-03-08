30370f9578088aad4b3fe4e37adf631cc8b204f4
Longyi Qi <lqi@uh.edu>
2011-03-07 23:16:40 -0600

    clean the PlainTextReporterTest

--- test/impl/mo/reporter/PlainTextReporterTest.cpp
+++ test/impl/mo/reporter/PlainTextReporterTest.cpp
@@ -3,6 +3,24 @@
 #include "mo/rule/MockRule.h"
 #include "mo/util/TestCursorUtil.h"
 
+vector<CXDiagnostic> getTestDiagnostics() {
+  CXIndex index = clang_createIndex(0, 0);
+  CXTranslationUnit translationUnit = clang_parseTranslationUnit(index, "test/samples/CompilerDiagnostics.cpp", 0, 0, 0, 0, CXTranslationUnit_None);
+  vector<CXDiagnostic> diagnostics;
+  for (int index = 0, numberOfDiagnostics = clang_getNumDiagnostics(translationUnit); index < numberOfDiagnostics; index++) {
+    diagnostics.push_back(clang_getDiagnostic(translationUnit, index));
+  }
+  return diagnostics;
+}
+
+vector<RuleViolation> getTestViolations() {
+  CXCursor switchStmtCursor = TestCursorUtil::getSwitchStmtCursor();
+  RuleViolation violation(switchStmtCursor, new MockRule());
+  vector<RuleViolation> violations;
+  violations.push_back(violation);
+  return violations;
+}
+
 void PlainTextReporterTest::setUp() {
   _reporter = new PlainTextReporter();
 }
@@ -17,15 +35,9 @@ void PlainTextReporterTest::testReportDiagnostics() {
   diagnosticMessage += "test/samples/CompilerDiagnostics.cpp:3:9: note: use '==' to turn this assignment into an equality comparison\n";
   diagnosticMessage += "test/samples/CompilerDiagnostics.cpp:3:9: note: place parentheses around the assignment to silence this warning\n";
   diagnosticMessage += "test/samples/CompilerDiagnostics.cpp:6:11: error: expected ';' after return statement\n";
-  CXIndex index = clang_createIndex(0, 0);
-  CXTranslationUnit translationUnit = clang_parseTranslationUnit(index, "test/samples/CompilerDiagnostics.cpp", 0, 0, 0, 0, CXTranslationUnit_None);
-  vector<CXDiagnostic> diagnostics;
-  for (int index = 0, numberOfDiagnostics = clang_getNumDiagnostics(translationUnit); index < numberOfDiagnostics; index++) {
-    diagnostics.push_back(clang_getDiagnostic(translationUnit, index));
-  }
+  
+  vector<CXDiagnostic> diagnostics = getTestDiagnostics();
   TS_ASSERT_EQUALS(_reporter->reportDiagnostics(diagnostics), diagnosticMessage);
-  clang_disposeTranslationUnit(translationUnit);
-  clang_disposeIndex(index);
 }
 
 void PlainTextReporterTest::testCursorLocationToPlainText() {
@@ -36,12 +48,7 @@ void PlainTextReporterTest::testCursorLocationToPlainText() {
 
 void PlainTextReporterTest::testReportViolations() {
   string violationMessage = "test/samples/SwitchStatement.m:3:3: code smell: mock rule\n";
-  violationMessage += "test/samples/SwitchStatement.m:3:3: code smell: mock rule\n";
-  CXCursor switchStmtCursor = TestCursorUtil::getSwitchStmtCursor();
-  RuleViolation violation1(switchStmtCursor, new MockRule());
-  RuleViolation violation2(switchStmtCursor, new MockRule());
-  vector<RuleViolation> violations;
-  violations.push_back(violation1);
-  violations.push_back(violation2);
+
+  vector<RuleViolation> violations = getTestViolations();  
   TS_ASSERT_EQUALS(_reporter->reportViolations(violations), violationMessage);
 }
