c26bd7808feb144506f27bef2527478bea2befcc
Longyi Qi <longyi@chaione.com>
2011-10-02 00:37:41 -0500

    reduce the complexity of UnusedLocalVariableRule

--- src/headers/mo/rule/UnusedLocalVariableRule.h
+++ src/headers/mo/rule/UnusedLocalVariableRule.h
@@ -3,9 +3,15 @@
 
 #include "mo/Rule.h"
 
+namespace clang {
+  class DeclContext;
+}
+
 class UnusedLocalVariableRule : public Rule {
 private:
   static RuleSet rules;
+  
+  bool isObjCInterfaceContainerDecl(clang::DeclContext *context);
   
 public:
   virtual void apply(CXCursor& node, CXCursor& parentNode, ViolationSet& violationSet);
--- src/impl/mo/rule/UnusedLocalVariableRule.cpp
+++ src/impl/mo/rule/UnusedLocalVariableRule.cpp
@@ -11,22 +11,24 @@ using namespace clang;
 
 RuleSet UnusedLocalVariableRule::rules(new UnusedLocalVariableRule());
 
+bool UnusedLocalVariableRule::isObjCInterfaceContainerDecl(DeclContext *context) {
+  return isa<ObjCInterfaceDecl>(context) || isa<ObjCProtocolDecl>(context) || isa<ObjCCategoryDecl>(context);
+}
+
 void UnusedLocalVariableRule::apply(CXCursor& node, CXCursor& parentNode, ViolationSet& violationSet) {
   Decl *decl = CursorUtil::getDecl(node);
   if (decl) {
     VarDecl *valDecl = dyn_cast<VarDecl>(decl);
-    if (valDecl) {
-      if (!valDecl->isUsed()) {
-        DeclContext *context = valDecl->getDeclContext();
-        do {
-          if (isa<ObjCInterfaceDecl>(context) || isa<ObjCProtocolDecl>(context) || isa<ObjCCategoryDecl>(context)) {
-            return;
-          }
-          context = context->getParent();
-        } while (context);
-        Violation violation(node, this);
-        violationSet.addViolation(violation);
+    if (valDecl && !valDecl->isUsed()) {
+      DeclContext *context = valDecl->getDeclContext();
+      while (context) {
+        if (isObjCInterfaceContainerDecl(context)) {
+          return;
+        }
+        context = context->getParent();
       }
+      Violation violation(node, this);
+      violationSet.addViolation(violation);
     }
   }
 }
