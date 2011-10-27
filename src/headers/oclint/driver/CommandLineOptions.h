eeb20d516f7922d022809f8c47e03d66f508dfcc
Longyi Qi <longyi@chaione.com>
2011-10-27 00:18:40 -0500

    started supporting command line options to configure the compiler, and multiple rule loading path and multiple input files are supported

---
+++ src/headers/oclint/driver/CommandLineOptions.h
@@ -0,0 +1,25 @@
+#include <string>
+#include <llvm/Support/CommandLine.h>
+
+using namespace std;
+using namespace llvm;
+using namespace cl;
+
+// input and output
+list<string> argInputs(Positional, desc("<input files>"), OneOrMore);
+//opt<string> argXCodeProject("xcode", desc("Specify the XCode project to inspect on"), value_desc("xcode project or workspace"), init("-"));
+//opt<string> argOutput("o", desc("Write output to <file>"), value_desc("file"), init("-"));
+//opt<string> argHtmlOutput("html", desc("Write HTML output to <file>"), value_desc("file"), init("-"));
+
+// compiler configuration
+list<string> argIncludeSearchPath("I", Prefix, desc("Add directory to include search path"), value_desc("directory"), ZeroOrMore);
+list<string> argFrameworkSearchPath("F", Prefix, desc("Add directory to framework include search path"), value_desc("directory"), ZeroOrMore);
+list<string> argMacros("D", Prefix, desc("Predefine the specified macro"), value_desc("macro"), ZeroOrMore);
+opt<string> argSysroot("isysroot", desc("Add directory to SYSTEM include search path"), value_desc("directory"), init("-"));
+list<string> argIncludes("include", desc("Include file before parsing"), value_desc("file"), ZeroOrMore);
+opt<string> argLanguageType("x", desc("Input language type"), value_desc("value"), init("-"));
+opt<string> argArch("arch", desc("Specify which architecture (e.g. ppc, i386, x86_64, armv7) the compilation should targets."), value_desc("arch_name"), init("-"));
+
+// oclint configuration
+list<string> argRulesPath("R", Prefix, cl::desc("Add directory to rule loading path"), cl::value_desc("directory"), ZeroOrMore);
+//list<string> argRuleConfiguration("rc", desc("Change the baheviour of rules"), value_desc("paramemter>=<value"), ZeroOrMore);
--- src/impl/main.cpp
+++ src/impl/main.cpp
@@ -1,26 +1,39 @@
 #include <dlfcn.h>
 #include <dirent.h>
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "oclint/ClangInstance.h"
 #include "oclint/SmellFinder.h"
 #include "oclint/RuleSet.h"
 #include "oclint/exception/GenericException.h"
 #include "oclint/reporter/PlainTextReporter.h"
+#include "oclint/driver/CommandLineOptions.h"
 
 using namespace std;
 
-int dynamicLoadRules(string executablePath, string relativeRulesPath) {
-  string absoluteRulesPath = executablePath.substr(0, executablePath.find_last_of("/") + 1) + relativeRulesPath;
-  
+static void versionPrinter() {
+  cout << "OCLint (http://oclint.org/):" << endl
+    << "  oclint version 0.2.4" << endl
+    << "  llvm version 3.0" << endl;
+}
+
+static void parseCommandLineOptions(int argc, char* argv[]) {
+  cl::SetVersionPrinter(versionPrinter);
+  cl::ParseCommandLineOptions(argc, argv, 
+    "OCLint, a static code analysis tool for Objective-C and related languages\n");
+}
+
+int dynamicLoadRules(string ruleDirPath) {
   DIR *dp;
   struct dirent *dirp;
 
-  dp = opendir(absoluteRulesPath.c_str());
+  dp = opendir(ruleDirPath.c_str());
   if (dp != NULL) {
     while ((dirp = readdir(dp))) {
       if (dirp->d_name[0] != '.') {
-        string rulePath = absoluteRulesPath + "/" + dirp->d_name;
+        string rulePath = ruleDirPath + "/" + string(dirp->d_name);
         void *ruleHandle = dlopen(rulePath.c_str(), RTLD_NOW);
         if(ruleHandle == NULL){
            cerr << dlerror() << endl;
@@ -32,6 +45,70 @@ int dynamicLoadRules(string executablePath, string relativeRulesPath) {
     closedir(dp);
   }
   return 0;
+}
+
+int consumeArgRulesPath(char* executablePath) {
+  if (argRulesPath.size() == 0) {
+    string exeStrPath(executablePath);
+    string defaultRulePath = exeStrPath.substr(0, exeStrPath.find_last_of("/") + 1) + "rules";
+    return dynamicLoadRules(defaultRulePath);
+  }
+  int returnFlag = 0;
+  for (unsigned i = 0; i < argRulesPath.size() && returnFlag == 0; ++i) {
+    returnFlag = dynamicLoadRules(argRulesPath[i]);
+  }
+  return returnFlag;
+}
+
+void consumeOptArgument(string argKey, string argValue, vector<string>& argVector) {
+  if (argValue != "-") {
+    argVector.push_back("-" + argKey);
+    argVector.push_back(argValue);
+  }
+}
+
+void consumeListArgument(string argKey, vector<string> argValues, vector<string>& argVector) {
+  for (unsigned i = 0; i < argValues.size(); ++i) {
+    argVector.push_back("-" + argKey);
+    argVector.push_back(argValues[i]);
+  }
+}
+
+void consumeOptArguments(vector<string>& argVector) {
+  consumeOptArgument("x", argLanguageType, argVector);
+  consumeOptArgument("arch", argArch, argVector);
+  consumeOptArgument("isysroot", argSysroot, argVector);
+}
+
+void consumeListArguments(vector<string>& argVector) {
+  consumeListArgument("D", argMacros, argVector);
+  consumeListArgument("F", argFrameworkSearchPath, argVector);
+  consumeListArgument("include", argIncludes, argVector);
+  consumeListArgument("I", argIncludeSearchPath, argVector);
+}
+
+vector<string> getCompilerArguments() {
+  vector<string> argv;
+  consumeOptArguments(argv);
+  consumeListArguments(argv);
+  return argv;
+}
+
+char* safeCStr(string str) {
+  char *argv = new char[str.size() + 1];
+  strncpy(argv, str.c_str(), str.size());
+  argv[str.size()] = '\0';
+  return argv;
+}
+
+char** getArgv(vector<string> argVector, string input) {
+  char** argv = new char*[argVector.size() + 1];
+  unsigned i;
+  for (i = 0; i < argVector.size(); i++) {
+    argv[i] = safeCStr(argVector[i]);
+  }
+  argv[i] = safeCStr(input);
+  return argv;
 }
 
 int reportSmells(ClangInstance& instance, PlainTextReporter& reporter) {
@@ -48,28 +125,39 @@ int reportSmells(ClangInstance& instance, PlainTextReporter& reporter) {
   return numberOfSmells;
 }
 
-int execute(const char * const * argv, int argc) {
+int execute() {
   PlainTextReporter reporter;
-  ClangInstance instance;
-  instance.compileSourceFileToTranslationUnit(argv, argc);
-  if (instance.hasErrors()) {
-    cout << instance.reportErrors(reporter);
-    return 1;
+  vector<string> argVector = getCompilerArguments();
+  int argc = argVector.size() + 1;
+  int totalNumberOfSmells = 0;
+  for (unsigned i = 0; i < argInputs.size(); i++) {
+    char** argv = getArgv(argVector, argInputs[i]);
+    ClangInstance instance;
+    instance.compileSourceFileToTranslationUnit(argv, argc);
+    if (instance.hasErrors()) {
+      cout << instance.reportErrors(reporter);
+      totalNumberOfSmells += 1;
+      continue;
+    }
+    totalNumberOfSmells += reportSmells(instance, reporter);
   }
-  return reportSmells(instance, reporter);
+  return totalNumberOfSmells;
 }
 
 int main(int argc, char* argv[]) {
-  int execute_flag = 99;
-  string executablePath(argv[0]);
-  execute_flag = dynamicLoadRules(executablePath, "rules");
-  if (execute_flag == 0) {
+  parseCommandLineOptions(argc, argv);
+  if (consumeArgRulesPath(argv[0]) == 0 && RuleSet::numberOfRules() > 0) {
     try {
-      execute_flag = execute(argv + 1, argc - 1);
+      return execute();
     }
     catch (GenericException& ex) {
       cout << "Exception: " << ex.message << endl;
+      return 97;
     }
   }
-  return execute_flag;
+  else {
+    cout << "No rule found" << endl;
+    return 98;
+  }
+  return 99;
 }
